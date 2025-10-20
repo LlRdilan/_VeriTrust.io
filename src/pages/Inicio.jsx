bf8c2f76a3c8822b6b6b6d5b5b862b765db4c99a
dilan <di.llanquileo@duocuc.cl>
2025-10-20 00:54:57 -0300

    Benevolencia

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,7 +5,7 @@ export default function Inicio() {
         <section className="banner_main">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-7 col-lg-7">
+                    <div className="col-md-7 col-lg-7 mx-auto">
                         <div className="text-bg">
                             <h1>Tu Identidad Digital,<br /></h1>
                             <span> más segura y confiable que nunca</span>
@@ -19,7 +19,7 @@ export default function Inicio() {
             <div className="business">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-7">
+                        <div className="col-md-7 mx-auto">
                             <div className="titlepage">
                                 <h2><strong className="yellow"></strong>Premios y nominaciones que nos motivan</h2>
                                 <p>Estos reconocimientos son el reflejo de la confianza de nuestros clientes y aliados, y nos impulsan a seguir desarrollando servicios digitales de excelencia.</p>
@@ -58,54 +58,58 @@ export default function Inicio() {
                     </div>
                 </div>
             </div>
-            <div class="portfolio">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12 ">
-                            <div class="titlepage">
-                                <h2 class="yellow"> Porque la confianza también se certifica.</h2>
+            <div className="portfolio">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 mx-auto">
+                            <div className="titlepage">
+                                <h2 className="yellow"> Porque la confianza también se certifica.</h2>
                                 <p>Cada una de estas alianzas refleja nuestro compromiso con la seguridad, la innovación y la confianza, permitiendo que miles de usuarios y organizaciones realicen sus gestiones digitales de forma simple y protegida.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div id="myCarousel" class="carousel slide portfolio_Carousel " data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+
+                <div id="myCarousel" className="carousel slide portfolio_Carousel" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
                         <li data-target="#myCarousel" data-slide-to="1"></li>
                         <li data-target="#myCarousel" data-slide-to="2"></li>
                     </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <div class="container">
-                                <div class="carousel-caption ">
-                                    <div class="row">
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <div className="container">
+                                <div className="carousel-caption">
+                                    <div className="row">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/cenco_pay_empresa.jpg" alt="#" />
-                                                <div class="middle">
-                                                    <div class="text2">View More</div>
+                                                <div className="middle">
+                                                    <div className="text2">View More</div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        {/* CORREGIDO: Error de tipeo 'clasclassNames' a 'className' */}
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/banco_falabella_empresa.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
+                                                {/* CORREGIDO: La ruta de la imagen va en 'src', no en 'className' */}
                                                 <img src="images/banco_santander_empresa.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/sii_foto.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
@@ -113,35 +117,36 @@ export default function Inicio() {
                                 </div>
                             </div>
                         </div>
-                        <div class="carousel-item">
-                            <div class="container">
-                                <div class="carousel-caption">
-                                    <div class="row">
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                        {/* CORREGIDO: Error de tipeo 'claclassNamess' a 'className' */}
+                        <div className="carousel-item">
+                            <div className="container">
+                                <div className="carousel-caption">
+                                    <div className="row">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/cenco_pay_empresa.jpg" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/banco_falabella_empresa.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/banco_santander_empresa.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-3 col-sm-6">
-                                            <div class="portfolio_img">
+                                        <div className="col-md-3 col-sm-6">
+                                            <div className="portfolio_img">
                                                 <img src="images/sii_foto.png" alt="#" />
-                                                <div class="middle">
+                                                <div className="middle">
                                                 </div>
                                             </div>
                                         </div>
@@ -150,11 +155,11 @@ export default function Inicio() {
                             </div>
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                    <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                        <i className="fa fa-chevron-left" aria-hidden="true"></i>
                     </a>
-                    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
                     </a>
                 </div>
             </div>
